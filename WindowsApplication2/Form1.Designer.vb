08998f586451e0cfdea6db81f98c7006b8dc2ebd
Rico - Laptop <federicoparassarmiento@yahoo.com>
2015-04-27 13:52:28 +0800

    INI

---
+++ WindowsApplication2/Form1.Designer.vb
@@ -0,0 +1,134 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lstFriends = New System.Windows.Forms.ListBox()
+        Me.btnArrayElement = New System.Windows.Forms.Button()
+        Me.btnEnumerateArray = New System.Windows.Forms.Button()
+        Me.btnArrayAsParameters = New System.Windows.Forms.Button()
+        Me.btnMoreArrayParameters = New System.Windows.Forms.Button()
+        Me.btnSortarrays = New System.Windows.Forms.Button()
+        Me.btnReversingAnArray = New System.Windows.Forms.Button()
+        Me.btnInitializingArraysWithValues = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lstFriends
+        '
+        Me.lstFriends.FormattingEnabled = True
+        Me.lstFriends.IntegralHeight = False
+        Me.lstFriends.Location = New System.Drawing.Point(0, 3)
+        Me.lstFriends.Name = "lstFriends"
+        Me.lstFriends.Size = New System.Drawing.Size(179, 259)
+        Me.lstFriends.TabIndex = 0
+        '
+        'btnArrayElement
+        '
+        Me.btnArrayElement.Location = New System.Drawing.Point(185, 12)
+        Me.btnArrayElement.Name = "btnArrayElement"
+        Me.btnArrayElement.Size = New System.Drawing.Size(133, 27)
+        Me.btnArrayElement.TabIndex = 1
+        Me.btnArrayElement.Text = "Array Element"
+        Me.btnArrayElement.UseVisualStyleBackColor = True
+        '
+        'btnEnumerateArray
+        '
+        Me.btnEnumerateArray.Location = New System.Drawing.Point(185, 45)
+        Me.btnEnumerateArray.Name = "btnEnumerateArray"
+        Me.btnEnumerateArray.Size = New System.Drawing.Size(133, 23)
+        Me.btnEnumerateArray.TabIndex = 2
+        Me.btnEnumerateArray.Text = "Enymerate Array"
+        Me.btnEnumerateArray.UseVisualStyleBackColor = True
+        '
+        'btnArrayAsParameters
+        '
+        Me.btnArrayAsParameters.Location = New System.Drawing.Point(185, 74)
+        Me.btnArrayAsParameters.Name = "btnArrayAsParameters"
+        Me.btnArrayAsParameters.Size = New System.Drawing.Size(133, 23)
+        Me.btnArrayAsParameters.TabIndex = 3
+        Me.btnArrayAsParameters.Text = "Array as Parameters"
+        Me.btnArrayAsParameters.UseVisualStyleBackColor = True
+        '
+        'btnMoreArrayParameters
+        '
+        Me.btnMoreArrayParameters.Location = New System.Drawing.Point(185, 103)
+        Me.btnMoreArrayParameters.Name = "btnMoreArrayParameters"
+        Me.btnMoreArrayParameters.Size = New System.Drawing.Size(133, 23)
+        Me.btnMoreArrayParameters.TabIndex = 4
+        Me.btnMoreArrayParameters.Text = "More Array Parameters"
+        Me.btnMoreArrayParameters.UseVisualStyleBackColor = True
+        '
+        'btnSortarrays
+        '
+        Me.btnSortarrays.Location = New System.Drawing.Point(185, 133)
+        Me.btnSortarrays.Name = "btnSortarrays"
+        Me.btnSortarrays.Size = New System.Drawing.Size(133, 23)
+        Me.btnSortarrays.TabIndex = 5
+        Me.btnSortarrays.Text = "Sorting Arrays"
+        Me.btnSortarrays.UseVisualStyleBackColor = True
+        '
+        'btnReversingAnArray
+        '
+        Me.btnReversingAnArray.Location = New System.Drawing.Point(185, 163)
+        Me.btnReversingAnArray.Name = "btnReversingAnArray"
+        Me.btnReversingAnArray.Size = New System.Drawing.Size(133, 23)
+        Me.btnReversingAnArray.TabIndex = 6
+        Me.btnReversingAnArray.Text = "Reversing an Array"
+        Me.btnReversingAnArray.UseVisualStyleBackColor = True
+        '
+        'btnInitializingArraysWithValues
+        '
+        Me.btnInitializingArraysWithValues.Location = New System.Drawing.Point(185, 193)
+        Me.btnInitializingArraysWithValues.Name = "btnInitializingArraysWithValues"
+        Me.btnInitializingArraysWithValues.Size = New System.Drawing.Size(133, 23)
+        Me.btnInitializingArraysWithValues.TabIndex = 7
+        Me.btnInitializingArraysWithValues.Text = "Initializing Array"
+        Me.btnInitializingArraysWithValues.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(330, 262)
+        Me.Controls.Add(Me.btnInitializingArraysWithValues)
+        Me.Controls.Add(Me.btnReversingAnArray)
+        Me.Controls.Add(Me.btnSortarrays)
+        Me.Controls.Add(Me.btnMoreArrayParameters)
+        Me.Controls.Add(Me.btnArrayAsParameters)
+        Me.Controls.Add(Me.btnEnumerateArray)
+        Me.Controls.Add(Me.btnArrayElement)
+        Me.Controls.Add(Me.lstFriends)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents lstFriends As System.Windows.Forms.ListBox
+    Friend WithEvents btnArrayElement As System.Windows.Forms.Button
+    Friend WithEvents btnEnumerateArray As System.Windows.Forms.Button
+    Friend WithEvents btnArrayAsParameters As System.Windows.Forms.Button
+    Friend WithEvents btnMoreArrayParameters As System.Windows.Forms.Button
+    Friend WithEvents btnSortarrays As System.Windows.Forms.Button
+    Friend WithEvents btnReversingAnArray As System.Windows.Forms.Button
+    Friend WithEvents btnInitializingArraysWithValues As System.Windows.Forms.Button
+
+End Class
---
+++ WindowsApplication2/Form1.vb
@@ -0,0 +1,86 @@
+﻿Public Class Form1
+    Private strFriends(4) As String
+
+    Private Sub lstFriends_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lstFriends.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub btnArrayElement_Click(sender As Object, e As EventArgs) Handles btnArrayElement.Click
+        ClearList()
+
+        Dim strFriends(4) As String
+
+        strFriends(0) = "Wendy"
+        strFriends(1) = "Harriet"
+        strFriends(2) = "Jay"
+        strFriends(3) = "Michelle"
+        strFriends(4) = "Richard"
+
+        lstFriends.Items.Add(strFriends(0))
+
+    End Sub
+
+    Private Sub ClearList()
+        lstFriends.Items.Clear()
+    End Sub
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        strFriends(0) = "Wendy"
+        strFriends(1) = "Harriet"
+        strFriends(2) = "Jay"
+        strFriends(3) = "Michelle"
+        strFriends(4) = "Richard"
+    End Sub
+
+    Private Sub btnEnumerateArray_Click(sender As Object, e As EventArgs) Handles btnEnumerateArray.Click
+        ClearList()
+
+        For Each strName As String In strFriends
+            lstFriends.Items.Add(strName)
+        Next
+    End Sub
+
+    Private Sub btnArrayAsParameters_Click(sender As Object, e As EventArgs) Handles btnArrayAsParameters.Click
+        ClearList()
+        AddItemsToList(strFriends)
+    End Sub
+
+    Private Sub AddItemsToList(ByVal arrayList() As String)
+        For Each strName As String In arrayList
+            lstFriends.Items.Add(strName)
+        Next
+    End Sub
+
+    Private Sub btnMoreArrayParameters_Click(sender As Object, e As EventArgs) Handles btnMoreArrayParameters.Click
+        'Clear the list
+        ClearList()
+        'Declare an array
+        Dim strMoreFriends(1) As String
+        'Populate the array
+        strMoreFriends(0) = "Elaine"
+        strMoreFriends(1) = "Debra"
+        'List your friends
+        AddItemsToList(strFriends)
+        AddItemsToList(strMoreFriends)
+    End Sub
+
+    Private Sub btnSortarrays_Click(sender As Object, e As EventArgs) Handles btnSortarrays.Click
+        ClearList()
+        Array.Sort(strFriends)
+        AddItemsToList(strFriends)
+    End Sub
+
+    
+    Private Sub btnReversingAnArray_Click(sender As Object, e As EventArgs) Handles btnReversingAnArray.Click
+        ClearList()
+        Array.Reverse(strFriends)
+        AddItemsToList(strFriends)
+    End Sub
+
+    Private Sub btnInitializingArraysWithValues_Click(sender As Object, e As EventArgs) Handles btnInitializingArraysWithValues.Click
+        ClearList()
+        Dim strMyFriends() As String = {"Elaine", "Richard", "Debra", "Wendy", "Harrier"}
+        AddItemsToList(strMyFriends)
+    End Sub
+End Class
